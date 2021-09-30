37c32ea6548bc8efe179d79996cf34276bba9ab7
AB Moreno <rappygod678@gmail.com>
2021-09-30 09:32:13 -0400

    //

--- Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
@@ -50,7 +50,7 @@ struct Height {
  Now create a variable instance of `Height` called `someonesHeight`. Use the initializer for inches to set the height to 65. Print out the property for height in centimeters and verify that it is equal to 165.1.
  */
 var someonesHeight = Height(heightInInches: 65)
-print(heightInCentimeters)m     `1
+print(heightInCentimeters)
 
 /*:
  Now create a variable instance of `Height` called `myHeight` and initialize it with your own height. Verify that both `heightInInches` and `heightInCentimeters` are accurate.
